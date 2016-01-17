35a94445d57377c49f8859a2114fcf3a61023896
kebz <chabinkapiotr@gmail.com>
2016-01-17 16:25:00 +0100

    Revert "Revert "Created web API for database""
    This reverts commit ee9546f19449261162a910a5836f83d19f68beab.

--- DjangoProj/klient/admin.py
+++ DjangoProj/klient/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from klient.models import Klient, Kontakt
 
+admin.site.register(Klient)
+admin.site.register(Kontakt)
 # Register your models here.
--- DjangoProj/platnosci/admin.py
+++ DjangoProj/platnosci/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from platnosci.models import Platnosc, Historia_platnosci
 
+admin.site.register(Platnosc)
+admin.site.register(Historia_platnosci)
 # Register your models here.
