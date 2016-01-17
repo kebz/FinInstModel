c7cc2403145f2d6244fd1840245789891173c209
siezyj <mateuszwozny4@gmail.com>
2016-01-17 22:45:24 +0100

    test Rudego ;*

--- DjangoProj/klient/admin.py
+++ DjangoProj/klient/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from klient.models import Klient, Kontakt
+from klient.models import Klient, Kontakt, Adres
 
 admin.site.register(Klient)
 admin.site.register(Kontakt)
+admin.site.register(Adres)
 # Register your models here.
