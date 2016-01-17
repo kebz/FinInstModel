da120d58ba5d449d1ef347d92b154408ccf68df9
kebz <chabinkapiotr@gmail.com>
2016-01-17 16:11:35 +0100

    Deleted some idiot tests for klient

--- DjangoProj/klient/tests.py
+++ DjangoProj/klient/tests.py
@@ -1,11 +1,7 @@
 from django.test import TestCase
 from klient.models import *
 
-class KlientTestCase(TestCase):
-    def test_klienta(self):
-        u1 = User.objects.create(username="kebz", first_name="piotr", last_name="chabinka")
-        k1 = Klient.objects.create(uzytkownik=u1, pesel = 0000000000)
-        piotr = Klient.objects.get(uzytkownik = u1)
+
 
 
 # Create your tests here.
