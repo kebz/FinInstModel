7b405fbbaadef9f906a0032396c6a11c7cca416c
kebz <chabinkapiotr@gmail.com>
2016-01-20 23:32:26 +0100

    porfel view was added to website

--- DjangoProj/portfel/admin.py
+++ DjangoProj/portfel/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from portfel.models import Portfel, Elementy_portfela
+
+admin.site.register(Portfel)
+admin.site.register(Elementy_portfela)
+
 
 # Register your models here.
--- DjangoProj/portfel/models.py
+++ DjangoProj/portfel/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from globalnewartosci.models import *
-from klient.models import *
+from katalog.models import Usluga
+from klient.models import Klient
 
 
-class Portfel(model.Model):
-    id_portfela=models.CharField(models.CharField( primary_key=True, null=False, max_length=500, default="PR"))
-    id_klienta=models.ForeignKey('Klient',to_field='id_klienta',on_delete=model.CASCADE)
-    wartosc=model.BigIntegerField(default=None)
+class Portfel(models.Model):
+    id_portfela=models.CharField( primary_key=True, null=False, max_length=500, default="PR")
+    id_klienta=models.ForeignKey('klient.Klient',to_field='id_klienta',on_delete=models.CASCADE)
+    wartosc=models.BigIntegerField(default=None)
 
-class Elementy_portfela(model.Model):
-    id_portfela= models.ForeignKey('Portfel',to_field='id_portfela', on_delete=model.CASCADE)
-    id_uslugi=models.ForeignKey('katalog.Usluga',to_field='id_uslugi', on_delete=model.CASCADE)
-    ilosc=model.BigIntegerField(default=None)
+class Elementy_portfela(models.Model):
+    id_portfela= models.ForeignKey('Portfel',to_field='id_portfela', on_delete=models.CASCADE, primary_key=True)#, max_length=500)
+    id_uslugi=models.ForeignKey('katalog.Usluga',to_field='id_uslugi', on_delete=models.CASCADE)
+    ilosc=models.BigIntegerField(default=None)
